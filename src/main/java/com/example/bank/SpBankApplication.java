073cf54eb9502e25b3455f5555956408d6173e2d
Prasanatma <prasanatmaanvekar@gmail.com>
2020-07-03 00:55:57 +0530

    hello message api created

--- src/main/java/com/example/bank/SpBankApplication.java
+++ src/main/java/com/example/bank/SpBankApplication.java
@@ -2,12 +2,24 @@ package com.example.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpBankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpBankApplication.class, args);
+	}
+	
+	@GetMapping("api/hello/{user}")
+	@ResponseBody
+	public String testMessage(@PathVariable String user) {
+		return "Hello "+ user + " welcome to first page....!!";
 	}
 
 }
