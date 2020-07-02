ddf6239dab06f2c0554d48c5729b833a46256ccf
Prasanatma <prasanatmaanvekar@gmail.com>
2020-07-03 00:42:03 +0530

    Initial Setup files added

---
+++ src/main/java/com/example/bank/SpBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpBankApplication.class, args);
+	}
+
+}
